f9c66e4159a636d98fe888dc399d66ead1400332
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-08-27 00:23:45 -0400

    Implement Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="app w-[80%] m-auto">
+      <Navbar />
+    </div>
   )
 }
 
---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { assets } from "../../assets/assets";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [menu, setMenu] = useState("home")
+
+  return (
+    <div className="py-[20px] px-0 flex justify-between items-center lowercase">
+      <img 
+        src={assets.logo} 
+        alt="Logo" 
+        className="w-[150px]"
+      />
+      <ul className="flex list-none gap-[20px] text-[#49557e] text-[18px]">
+        <li 
+        onClick={() => setMenu("home")} 
+        className={`cursor-pointer duration-300 ${menu === "home" ? "pb-[2px] border-b-2 border-[#49557e]" : ""}`}>
+          Home
+        </li>
+        <li 
+        onClick={() => setMenu("menu")} 
+        className={`cursor-pointer duration-300 ${menu === "menu" ? "pb-[2px] border-b-2 border-[#49557e]" : ""}`}>
+          Menu
+        </li>
+        <li 
+        onClick={() => setMenu("mobile-app")} 
+        className={`cursor-pointer duration-300 ${menu === "mobile-app" ? "pb-[2px] border-b-2 border-[#49557e]" : ""}`}>
+          Mobile-App
+        </li>
+        <li 
+        onClick={() => setMenu("contact")} 
+        className={`cursor-pointer duration-300 ${menu === "contact" ? "pb-[2px] border-b-2 border-[#49557e]" : ""}`}>
+          Contact
+        </li>
+      </ul>
+      <div className="flex items-center gap-[40px]">
+        <img src={assets.search_icon} alt="search" />
+        <div className="relative">
+          <img src={assets.basket_icon} alt="cart" />
+          <div className="absolute min-w-[10px] min-h-[10px] bg-[#ff6347] rounded-[5px] top-[-8px] right-[-8px]"></div>
+        </div>
+        <button 
+        className="lowercase bg-transparent text-[16px] text-[#49557e] border border-[#ff6347] py-[10px] px-[30px] cursor-pointer rounded-[50px] duration-300 hover:bg-[#fff4f2]">
+          Sign In
+          </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
